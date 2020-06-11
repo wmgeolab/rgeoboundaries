b3b50bf5da03367585a4f023d265ebdbcf814eb6
Ahmadou Dicko <mail@ahmadoudicko.com>
2020-06-11 20:40:31 +0000

    Add new tests

--- tests/testthat/test-geoboundaries.R
+++ tests/testthat/test-geoboundaries.R
@@ -1,18 +1,23 @@
-context("gb_countries: test types and errors")
+context("geoboundaries: test types and errors")
 library(sf)
 
 test_that("type of object returned is as expected", {
-  skip_on_cran()
   p <- gb_adm0(country = "Mali")
   expect_is(p, "sf")
   expect_true(st_geometry_type(p) %in% c("MULTIPOLYGON", "POLYGON"))
 })
 
 test_that("simplified boundaries take less size on memory", {
-  skip_on_cran()
   p1 <- gb_adm0(country = "Cabo Verde", type = "hpscu")
   p2 <- gb_adm0(country = "Cabo Verde", type = "sscu")
 
   expect_gt(object.size(p1),
             object.size(p2))
+})
+
+test_that("Downloaded data are cached", {
+  gb_clear_cache()
+  expect_equal(length(gb_list_cache()), 0)
+  p <- gb_adm0(country = "Mali")
+  expect_gte(length(gb_list_cache()), 1)
 })
